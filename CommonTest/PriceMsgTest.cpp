481d60dfbbbcd75697d0c08c3a472bea9b1f6c5a
Yoonbae Cho <y@xcv.kr>
2019-05-27 23:25:15 +0900

    add test for RESET msg

--- CommonTest/PriceMsgTest.cpp
+++ CommonTest/PriceMsgTest.cpp
@@ -7,49 +7,54 @@
 using namespace std;
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace CommonTest
+TEST_CLASS(PriceMsgTest)
 {
-	TEST_CLASS(PriceMsgTest)
+public:
+	TEST_METHOD(Correct)
 	{
-	public:
-		TEST_METHOD(Correct)
-		{
-			auto msg = "015760 4000 10 1234512345";
-			auto m = PriceMsg::Parse(msg);
+		auto msg = "015760 4000 10 1234512345";
+		auto m = PriceMsg::Parse(msg);
 
-			auto s = string("015760");
-			double p = 4000.0;
-			size_t v = 10;
-			time_t t = 1234512345;
+		auto s = string("015760");
+		double p = 4000.0;
+		size_t v = 10;
+		time_t t = 1234512345;
 
-			Assert::AreEqual(m.symbol, s);
-			Assert::AreEqual(m.price, p);
-			Assert::AreEqual(m.volume, v);
-			Assert::IsTrue(m.timestamp == t);
-		}
+		Assert::AreEqual(m.symbol, s);
+		Assert::AreEqual(m.price, p);
+		Assert::AreEqual(m.volume, v);
+		Assert::IsTrue(m.timestamp == t);
+	}
 
-		TEST_METHOD(EmptyMessage)
-		{
-			auto line = "";
-			auto func = [line] { PriceMsg::Parse(line); };
+	TEST_METHOD(EmptyMessage)
+	{
+		auto line = "";
+		auto func = [line] { PriceMsg::Parse(line); };
 
-			Assert::ExpectException<ParsingException>(func);
-		}
+		Assert::ExpectException<ParsingException>(func);
+	}
 
-		TEST_METHOD(WrongMessage)
-		{
-			auto line = "WRONG WRONG WRONG";
-			auto func = [line] { PriceMsg::Parse(line); };
+	TEST_METHOD(WrongMessage)
+	{
+		auto line = "WRONG WRONG WRONG";
+		auto func = [line] { PriceMsg::Parse(line); };
 
-			Assert::ExpectException<ParsingException>(func);
-		}
+		Assert::ExpectException<ParsingException>(func);
+	}
 
-		TEST_METHOD(QuitMessage)
-		{
-			auto line = "QUIT";
-			auto func = [line] { PriceMsg::Parse(line); };
+	TEST_METHOD(ResetMessage)
+	{
+		auto line = "RESET";
+		auto func = [line] { PriceMsg::Parse(line); };
 
-			Assert::ExpectException<QuitException>(func);
-		}
-	};
-}
+		Assert::ExpectException<ResetException>(func);
+	}
+
+	TEST_METHOD(QuitMessage)
+	{
+		auto line = "QUIT";
+		auto func = [line] { PriceMsg::Parse(line); };
+
+		Assert::ExpectException<QuitException>(func);
+	}
+};
